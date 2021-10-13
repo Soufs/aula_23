c8426c041db37676b4c202ed904fcc6b83d75ffa
Soufs <91095207+Soufs@users.noreply.github.com>
2021-10-13 19:33:02 -0300

    Add files via upload

--- box.js
+++ box.js
@@ -1,8 +1,7 @@
 class Box {
     constructor(x,y,width,height){
         var options = {
-            'restitution': 0.8
-        }
+            restitutio: 1        }
 
         this.body = Bodies.rectangle(x,y,width,height, options);
         this.width = width;
@@ -12,13 +11,9 @@ class Box {
 
     display(){
         var pos = this.body.position;
-        var angle = this.body.angle;
-        push();
-        translate(pos.x,pos.y);
-        rotate(angle);
         rectMode(CENTER);
         fill(255);
-        rect(0,0,this.width,this.height);
-        pop();
+        rect(pos.x, pos.y,this.width,this.height);
     }
+
 }
--- sketch.js
+++ sketch.js
@@ -3,19 +3,15 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var caixa1, caixa2;
-var solo1;
+var caixa1;
 
 function setup(){
     var canvas = createCanvas(400,400);
     engine = Engine.create();
     world = engine.world;
 
-    caixa1 = new Box(200,300,50,50);
-    caixa2 = new Box(240,100,50,100);
-
-    solo1 = new Solo(200,390,400,20);
-
+    caixa1 = new Box(200,100,50,50);
+    
 }
 
 function draw(){
@@ -23,8 +19,5 @@ function draw(){
     Engine.update(engine);
 
     caixa1.display();
-    caixa2.display();
-
-    solo1.display();
-
+    
 }
